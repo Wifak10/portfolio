2ed051ac88ecb6bd9a5de49e21ece5aa64413002
wifak10 <wifakayekpola123@gmail.com>
2025-03-21 15:30:46 +0100

    Initialisation du projet

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  // ui: {
+  //   icons: ['mdi'], // Ajoute des icônes Material Design
+  // },
+  // devtools: { enabled: true },
+})
